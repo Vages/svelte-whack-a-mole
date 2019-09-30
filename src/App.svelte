bdb1f42e0f6bb856de712fb8c6a5bec35380de90
Eirik Vageskar <eirikvageskar@gmail.com>
2019-09-30 22:17:39 +0200

    Add some sweet mice

--- src/App.svelte
+++ src/App.svelte
@@ -9,43 +9,71 @@
     score += 1;
   }
 
-  const positions = [...Array(16).keys()];
+  const NUMBER_OF_MOLES = 16;
+  const POSITIONS = [...Array(NUMBER_OF_MOLES).keys()];
+  const MAX_RETRACT_TIME = 5000;
 
-  const moles = new Set([...Array(16).keys()]);
+  let moles = new Set([]);
+  // let moles = new Set([...Array(NUMBER_OF_MOLES).keys()]);
 
-  const killMole = moleNumber => () => {
+  const whackMole = moleNumber => () => {
     moles.delete(moleNumber);
     score += 1;
     moles = moles;
   };
+
+  const addMole = () => {
+    const newMole = Math.floor(Math.random() * NUMBER_OF_MOLES);
+    moles.add(newMole);
+    const timeBeforeItRectracts = Math.floor(Math.random() * MAX_RETRACT_TIME);
+    setTimeout(moleRectracts(newMole), timeBeforeItRectracts);
+    moles = moles;
+  };
+
+  const moleRectracts = moleNumber => () => {
+    moles.delete(moleNumber);
+    moles = moles;
+  };
+
+  setInterval(addMole, 1000);
 </script>
 
 <style>
   .field {
+    /*background-color: green;*/
     display: grid;
     grid-gap: 5px;
     grid-template-columns: repeat(4, min-content);
   }
 
   .hill {
-    background-color: red;
+    font-size: 80px;
     width: 80px;
     height: 80px;
   }
   .mole {
+    font-size: 50px;
+    position: relative;
+    top: -120px;
+    border: none;
+    background: none;
+    padding: 0;
+    width: 80px;
+    height: 80px;
   }
 </style>
 
 {score}
 <div class="field">
-  {#each positions as position}
+  {#each POSITIONS as position}
     <div class="hill">
+      🗻
       {#if moles.has(position)}
         <button
-          transition:fly={{ y: 80, opacity: 100 }}
-          on:click={killMole(position)}
-          class="mole">
-          mole
+          class="mole"
+          transition:fly={{ y: 80 }}
+          on:click={whackMole(position)}>
+          🐭
         </button>
       {/if}
     </div>
