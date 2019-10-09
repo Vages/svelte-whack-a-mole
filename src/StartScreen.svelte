543eb40ed0b1b92d3a127478fc185c49bf088132
Eirik Vageskar <eirikvageskar@gmail.com>
2019-10-09 12:30:32 +0200

    Add a start screen

--- src/StartScreen.svelte
+++ src/StartScreen.svelte
@@ -8,4 +8,42 @@
   };
 </script>
 
-<button on:click={pressStart} >Start</button>
+<style>
+  .presenting {
+    font-size: 1.8rem;
+    font-weight: 400;
+  }
+
+  .game-name {
+    text-transform: uppercase;
+    font-size: 5rem;
+    letter-spacing: 0.15em;
+  }
+
+  .container {
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    margin: 0 auto;
+    max-width: 22rem;
+    justify-content: center;
+  }
+
+  img {
+    transform: rotate(15deg);
+    position: relative;
+    float: right;
+    width: 90px;
+  }
+</style>
+
+<div class="container">
+  <div class="presenting">Knowit presenterer</div>
+  <div>
+    <h1 class="game-name">
+      <img alt="Mus" src="mouse-face.png" />
+      Mos ei mus
+    </h1>
+  </div>
+  <button class="main-button" on:click={pressStart}>Start spill</button>
+</div>
